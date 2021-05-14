a10787727b5d43046efb6e1be99c03bbfa1bcdde
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-05-13 20:30:35 -0400

    Basic Query to backend

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,24 @@
 import React from 'react'
+import { useLazyQuery } from '@apollo/client'
+import { GET_WEATHER } from '../graphql/Queries'
 
-const dashboard = () => {
+const Dashboard = () => {
+    const [weatherQuery, {loading, data, error}] = useLazyQuery(GET_WEATHER, {
+        variables: { name: "Waterloo"}
+    })
+
+    if (error) return <h1>Error Found</h1>
+    if (data) {
+        console.log(data)
+    }
+
     return (
         <div className="home">
             <h1>Search For Weather</h1>
             <input type="text" placeholder="City name..." />
-            <button>Search</button>
+            <button onClick={() => weatherQuery()}>Search</button>
         </div>
     )
 }
 
-export default dashboard
+export default Dashboard
