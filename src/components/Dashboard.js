55e802b40cd6bcf50e7a498d24af400a32406213
ethanalvizo <55671206+ethanalvizo@users.noreply.github.com>
2021-05-13 20:40:11 -0400

    Dynamic search querying

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLazyQuery } from '@apollo/client'
 import { GET_WEATHER } from '../graphql/Queries'
 
 const Dashboard = () => {
+    const [search, setSearch] = useState("");
     const [weatherQuery, {loading, data, error}] = useLazyQuery(GET_WEATHER, {
-        variables: { name: "Waterloo"}
+        variables: { name: search }
     })
 
     if (error) return <h1>Error Found</h1>
@@ -15,8 +16,9 @@ const Dashboard = () => {
     return (
         <div className="home">
             <h1>Search For Weather</h1>
-            <input type="text" placeholder="City name..." />
+            <input type="text" placeholder="City name..." onChange={(event) => { setSearch(event.target.value) }} />
             <button onClick={() => weatherQuery()}>Search</button>
+
         </div>
     )
 }
